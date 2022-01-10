2873b15e4509eb92bd49ea2253c23718c9f612bf
Wang Wenhan <wwhjacob@hotmail.com>
2022-01-10 23:14:06 +0800

    Update treetransformernew.py

--- treetransformernew.py
+++ treetransformernew.py
@@ -11,7 +11,7 @@ import dgl
 import dgl.function as fn
 from dgl.nn import GlobalAttentionPooling
 from propagate import prop_nodes_topdown
-#from classifier import BasicTransformer, BasicTransformerEncoder
+from modules.attention import MultiHeadAttention_TUPE
 from copy import deepcopy
 import numpy as np
 
@@ -36,32 +36,17 @@ class ScaledDotProductAttention(nn.Module):
         # v: [b_size x n_heads x len_v x d_v] note: (len_k == len_v)
 
         # attn: [b_size x n_heads x len_q x len_k]
-        #print(q.size(),k.size(),v.size())
         scores = torch.matmul(q, k.transpose(-1, -2)) / self.scale_factor
         if attn_mask is not None:
             assert attn_mask.size() == scores.size()
             scores.masked_fill_(attn_mask, -1e9)
-        #print(scores.size())
-        #print(scores)
-        #print(self.softmax(scores))
         if self.top_down==True:
-            #newsoftmax = nn.Softmax(dim=-2)
-            #print(scores)
-            #print(self.softmax(scores))
-            #attn=self.dropout(F.sigmoid(scores))
-            attn = self.dropout(self.softmax(scores))
-            #print(attn.size(),v.size())
-            #print(attn)
-            #quit()
-            #attn=self.dropout(scores)           
+            attn = self.dropout(self.softmax(scores))        
             context = torch.matmul(attn, v)
         else:
             attn = self.dropout(self.softmax(scores))
             context = torch.matmul(attn, v)
-        #print(attn)
         # outputs: [b_size x n_heads x len_q x d_v]
-        #print(context)
-        #quit()
         return context, attn
 
 
@@ -99,38 +84,6 @@ class MultiHeadAttention(nn.Module):
         # return the context and attention weights
         return context, attn
 
-class MultiBranchAttention(nn.Module):
-    def __init__(self, d_k, d_v, d_model, n_branches, dropout):
-        super(MultiBranchAttention, self).__init__()
-        self.d_k = d_k
-        self.d_v = d_v
-        self.d_model = d_model
-        self.n_branches = n_branches
-
-        self.multihead_attn = MultiHeadAttention(d_k, d_v, d_model, n_branches, dropout)
-        # additional parameters for BranchedAttention
-        self.w_o = nn.ModuleList([nn.Linear(d_v, d_model) for _ in range(n_branches)])
-        self.w_kp = torch.rand(n_branches)
-        self.w_kp = nn.Parameter(self.w_kp/self.w_kp.sum())
-        self.w_a = torch.rand(n_branches)
-        self.w_a = nn.Parameter(self.w_a/self.w_a.sum())
-
-        #self.pos_ffn = nn.ModuleList([PoswiseFeedForwardNet(d_model, d_ff//n_branches, dropout) for _ in range(n_branches)])
-        #self.dropout = nn.Dropout(dropout)
-        #self.layer_norm = LayerNormalization(d_model)
-        #nn.init.xavier_normal(self.w_o)
-
-    def forward(self, q, k, v, attn_mask=None):
-        # context: a tensor of shape [b_size x len_q x n_branches * d_v]
-        context, attn = self.multihead_attn(q, k, v, attn_mask=attn_mask)
-
-        # context: a list of tensors of shape [b_size x len_q x d_v] len: n_branches
-        context = context.split(self.d_v, dim=-1)
-
-        # outputs: a list of tensors of shape [b_size x len_q x d_model] len: n_branches
-        outputs = [self.w_o[i](context[i]) for i in range(self.n_branches)]
-        outputs = [kappa * output for kappa, output in zip(self.w_kp, outputs)]
-        return outputs, attn
 
 class PositionwiseFeedForward(nn.Module):
     def __init__(self, dim_model, dim_ff, dropout=0.1):
@@ -227,8 +180,10 @@ class TreeTransformerCell(nn.Module):
         self.v_linear_p = nn.Linear(dim_model, dim_model)
 
 
-        self.sib_attention=MultiHeadAttention(self.d_k,self.d_k,self.dim_model,num_heads,dropout)
-        self.parent_attention=MultiHeadAttention(self.d_k,self.d_k,self.dim_model,num_heads,dropout)
+        #self.sib_attention=MultiHeadAttention(self.d_k,self.d_k,self.dim_model,num_heads,dropout)
+        self.sib_attention_tupe=MultiHeadAttention_TUPE(self.d_k,self.d_k,self.dim_model,num_heads,dropout,tupe=True,pe_mode='learned')
+        #self.parent_attention=MultiHeadAttention(self.d_k,self.d_k,self.dim_model,num_heads,dropout)
+        self.parent_attention=MultiHeadAttention_TUPE(self.d_k, self.d_k, self.dim_model, num_heads, dropout,tupe=False)
         self.ff=PositionwiseFeedForward(dim_model,dim_ff,dropout=dropout)
         self.ff.w_1.weight.data.uniform_(-0.1, 0.1)
         self.ff.w_2.weight.data.uniform_(-0.1, 0.1)
@@ -244,22 +199,24 @@ class TreeTransformerCell(nn.Module):
 
     def reduce_func(self, nodes):
         x=nodes.mailbox['h']
-        assert self.position_encoding is not None
-        if self.bottom_up_pos:
-            x=self.position_encoding(x) #sibling position
+        
+        q=self.q_linear(x) #sibling self_attention, can be removed
+        k=self.k_linear(x)
+        v=self.v_linear(x)
+        residual=x       
+        x,attn=self.sib_attention_tupe(q,k,v) #absolute position encoding with tupe        
+        x=self.layer_norm(x+residual)
 
         new_k=self.k_linear_p(x) #parent_children attention
         new_v=self.v_linear_p(x)
         parent_q=self.q_linear_p(nodes.data['h'].unsqueeze(1))
-
         residual_parent=parent_q.squeeze(1)
-
         parent_context, attn=self.parent_attention(parent_q,new_k,new_v) # context: [b_size x len_q(1) x dim] attn: [b_size x n_heads x len_q x len_v]
         parent_context=parent_context.squeeze(1)
         parent_context=self.layer_norm(parent_context+residual_parent)
+        
         h=self.ff(parent_context)
         h=self.layer_norm(h)
-     
         return {'h': h}
 
     def apply_node_func(self, nodes):
@@ -273,7 +230,7 @@ class TreeTransformerCell_topdown(nn.Module):
         self.dim_model = dim_model
         self.d_k = dim_model // num_heads
         self.h = num_heads
-        self.pos_enc='fixed'
+        '''self.pos_enc='fixed'
         print('position encoding:',self.pos_enc)
         assert self.pos_enc in ['fixed','learned']
         if self.pos_enc=='fixed':
@@ -281,7 +238,7 @@ class TreeTransformerCell_topdown(nn.Module):
         elif self.pos_enc=='learned':               
             self.position_encoding=learned_posenc
         self.top_down_pos=False
-        print('top down position',self.top_down_pos)
+        print('top down position',self.top_down_pos)'''
         # W_q, W_k, W_v, W_o
         self.k_linear = nn.Linear(dim_model, dim_model)
         self.q_linear = nn.Linear(dim_model, dim_model)
@@ -308,24 +265,13 @@ class TreeTransformerCell_topdown(nn.Module):
 
     def reduce_func(self, graph, nodes):
         x=nodes.mailbox['h']
-        if self.top_down_pos:
-            x=self.position_encoding(x) #sibling position encoding
-
-        #assert self.position_encoding is not None #position encoding before attention
-        #x=self.position_encoding(x)
-
-        new_k=self.k_linear_p(nodes.data['h'].unsqueeze(1)) #parent_children attention
-        new_v=self.v_linear_p(nodes.data['h'].unsqueeze(1))
-        children_q=self.q_linear_p(x)
-
-        residual_children=children_q
-
-        children_context, attn=self.parent_attention(children_q,new_k,new_v) # context: [b_size x len_q(1) x dim] attn: [b_size x n_heads x len_q x len_v]
-        #print(children_context.size(),residual_children.size())
+        child_num=x.size()[1]
+        residual_children=x        
+        children_context=nodes.data['h'].unsqueeze(1).expand(-1,child_num,-1)
         children_context=self.layer_norm(children_context+residual_children)
+        
         h=self.ff(children_context)
         h=self.layer_norm(h)
-
         graph.ndata['h'][nodes.mailbox['children_ids']]=h
         return {'h': nodes.data['h']}
 
@@ -366,9 +312,8 @@ class TreeTransformerClassifier(torch.nn.Module):
         #self.cell.ff.w_1.weight.data.uniform_(-initrange, initrange)
         #self.cell.ff.w_2.weight.data.uniform_(-initrange, initrange)
 
-    def forward(self, batch,root_ids=None):
+    def forward(self, batch):
         batch.ndata['h']=self.embeddings(batch.ndata['type'])
-        #batch.ndata['h']=self.cell.position_encoding.position_encoding_by_index(batch.ndata['h'],dfs_positions) #dfs position encoding (do not use)
 
         for i in range(self.num_stacks):
             dgl.prop_nodes_topo(batch,
@@ -380,43 +325,10 @@ class TreeTransformerClassifier(torch.nn.Module):
                                     message_func=self.cell_topdown.message_func,
                                     reduce_func=self.cell_topdown.reduce_func,
                                     apply_node_func=self.cell_topdown.apply_node_func)
-
-        if self.pool_mode=='root':
-            batch_pred=batch.ndata['h'][root_ids]
-        else:
-            batch_pred=self.pooling(batch,batch.ndata['h'])
+       
+        batch_pred=self.pooling(batch,batch.ndata['h'])
         batch_pred=self.classifier(batch_pred)
         return batch_pred
-    
-    def forward_nodeclassification(self, batch, node_ids, node_labels, root_ids, root_labels): #not used
-        batch.ndata['h']=self.embeddings(batch.ndata['name'])
-        #batch.ndata['h']=self.cell.position_encoding.position_encoding_by_index(batch.ndata['h'],dfs_positions) #dfs position encoding
-
-        for i in range(self.num_stacks):
-            dgl.prop_nodes_topo(batch,
-                                message_func=self.cell.message_func,
-                                reduce_func=self.cell.reduce_func,
-                                apply_node_func=self.cell.apply_node_func)
-            '''if self.top_down:
-                prop_nodes_topdown(batch,
-                                    message_func=self.cell_topdown.message_func,
-                                    reduce_func=self.cell_topdown.reduce_func,
-                                    apply_node_func=self.cell_topdown.apply_node_func)'''
-            #print(batch.ndata['h'].size(),node_ids.size(),root_ids.size())
-        h=batch.ndata.pop('h')
-        h=F.dropout(h,p=0.5)
-        node_hs=h[node_ids]
-        nodes_pred=self.classifier(node_hs)
-        #print(nodes_pred.size())
-        '''if self.pool_mode=='root':
-            batch_pred=batch.ndata['h'][root_ids]
-        else:
-            batch_pred=self.pooling(batch,batch.ndata['h'])
-        #print(batch_pred.size())'''
-        sents_hs=h[root_ids]
-        #sents_hs=self.pooling(batch,batch.ndata['h'])
-        sents_pred=self.classifier(sents_hs)
-        return nodes_pred,sents_pred
 
 
 class TreeTransformerEncoder(torch.nn.Module):
@@ -449,12 +361,7 @@ class TreeTransformerEncoder(torch.nn.Module):
         #self.cell.ff.w_2.weight.data.uniform_(-initrange, initrange)
 
     def forward(self, batch,root_ids=None):
-        #print(batch.ndata['type'].size())
         batch.ndata['h']=self.embeddings(batch.ndata['type'])
-        #dfs_positions=batch.ndata['dfs_id']
-        #batch.ndata['h']=self.cell.position_encoding.position_encoding_by_index(batch.ndata['h'],dfs_positions) #dfs position encoding
-        #batch.ndata['q']=self.cell.q_linear(batch.ndata['h'])
-        #batch.ndata['q']=batch.ndata['q'].view(-1,self.num_heads,self.cell.d_k)
         dgl.prop_nodes_topo(batch,
                             message_func=self.cell.message_func,
                             reduce_func=self.cell.reduce_func,
@@ -464,7 +371,6 @@ class TreeTransformerEncoder(torch.nn.Module):
                                 message_func=self.cell_topdown.message_func,
                                 reduce_func=self.cell_topdown.reduce_func,
                                 apply_node_func=self.cell_topdown.apply_node_func)
-            #quit()
         return batch
 
 class TreeTransformer_localize(torch.nn.Module):
@@ -482,18 +388,8 @@ class TreeTransformer_localize(torch.nn.Module):
         graph_probs=list(probs.split(batch_numnodes))
         graph_probs=pad_sequence(graph_probs,batch_first=True,padding_value=-1e9)
         graph_probs=F.log_softmax(graph_probs,dim=1)
-
-        '''probs= dgl.softmax_nodes(batch, feat='prob') #apply softmax_nodes without log
-        #probs=probs.log()
-        graph_probs=list(probs.split(batch_numnodes))
-        graph_probs=pad_sequence(graph_probs,batch_first=True,padding_value=0)'''
-
-        '''for i in range(len(graph_probs)):
-            graph_probs[i]=torch.cat([graph_probs[i],torch.zeros(batch_maxlen-batch_numnodes[i]).to(batch.device)])
-            print(graph_probs[i].size())
-        graph_probs=torch.stack(graph_probs)'''
-        #print(graph_probs.size())
         return graph_probs
+
 
 class TreeTransformer_typeandtoken(torch.nn.Module):
     def __init__(self, num_heads, dim_model, d_k, d_v, dim_hidden, n_classes, token_vocabsize, type_vocabsize, dropout=0.2, num_stacks=1,top_down=True):
@@ -532,10 +428,8 @@ class TreeTransformer_typeandtoken(torch.nn.Module):
         #self.cell.ff.w_1.weight.data.uniform_(-initrange, initrange)
         #self.cell.ff.w_2.weight.data.uniform_(-initrange, initrange)
 
-    def forward(self, batch,root_ids=None):
+    def forward(self, batch):
         batch.ndata['h']=torch.cat([self.type_embeddings(batch.ndata['type']),self.token_embeddings(batch.ndata['token'])],dim=1)
-        #dfs_positions=batch.ndata['dfs_id']
-        #batch.ndata['h']=self.cell.position_encoding.position_encoding_by_index(batch.ndata['h'],dfs_positions) #dfs position encoding
 
         for i in range(self.num_stacks):
             dgl.prop_nodes_topo(batch,
@@ -547,106 +441,6 @@ class TreeTransformer_typeandtoken(torch.nn.Module):
                                     message_func=self.cell_topdown.message_func,
                                     reduce_func=self.cell_topdown.reduce_func,
                                     apply_node_func=self.cell_topdown.apply_node_func)
-
-        if self.pool_mode=='root':
-            batch_pred=batch.ndata['h'][root_ids]
-        else:
-            batch_pred=self.pooling(batch,batch.ndata['h'])
+        batch_pred=self.pooling(batch,batch.ndata['h'])
         batch_pred=self.classifier(batch_pred)
         return batch_pred
-
-
-class TreeTransformer_codesearch(torch.nn.Module): #not used
-    def __init__(self, num_heads, dim_model, d_k, d_v, dim_hidden, vocab_size, dropout=0.2, num_stacks=1):
-        super(TreeTransformer_codesearch, self).__init__()
-        self.num_heads=num_heads
-        self.dim_model=dim_model
-        self.dropout = torch.nn.Dropout(dropout)
-        self.cell=TreeTransformerCell(num_heads,dim_model,dim_ff=dim_hidden,dropout=dropout)
-        self.cell_topdown=TreeTransformerCell_topdown(num_heads,dim_model,dim_ff=dim_hidden,dropout=dropout)
-        self.embeddings=nn.Embedding(vocab_size,dim_model)
-        self.num_stacks = num_stacks
-        self.pooling=GlobalAttentionPooling(nn.Linear(dim_model,1))
-        self.top_down=True
-        print('top_down',self.top_down)
-        self.pool_mode='attention'
-        print(self.pool_mode)
-        self.nlencoder=BasicTransformerEncoder(self.embeddings, dim_model, num_heads, 1024, nlayers=4, dropout=dropout)
-        self.dense = nn.Linear(dim_model, dim_model)
-        self.activation = nn.Tanh()
-        self.init_weights()
-    
-    def init_weights(self):
-        initrange = 0.1
-        self.embeddings.weight.data.uniform_(-initrange, initrange)
-        self.cell.k_linear.weight.data.uniform_(-initrange, initrange)
-        self.cell.q_linear.weight.data.uniform_(-initrange, initrange)
-        self.cell.v_linear.weight.data.uniform_(-initrange, initrange)
-        self.cell.k_linear_p.weight.data.uniform_(-initrange, initrange)
-        self.cell.q_linear_p.weight.data.uniform_(-initrange, initrange)
-        self.cell.v_linear_p.weight.data.uniform_(-initrange, initrange)
-        #self.cell.ff.w_1.weight.data.uniform_(-initrange, initrange)
-        #self.cell.ff.w_2.weight.data.uniform_(-initrange, initrange)
-
-    def outputlinear(self,encodings):
-        encodings=encodings[0,:,:] #get output of the first token
-        pooled_output = self.dense(encodings)
-        pooled_output = self.activation(pooled_output)
-        return pooled_output
-
-    def forward(self, batch,nlbatch, nl_padding_mask, root_ids=None):
-        bs=nlbatch.size()[0]
-        batch.ndata['h']=self.embeddings(batch.ndata['type'])
-        dfs_positions=batch.ndata['dfs_id']
-        #batch.ndata['h']=self.cell.position_encoding.position_encoding_by_index(batch.ndata['h'],dfs_positions) #dfs position encoding
-
-        for i in range(self.num_stacks):
-            dgl.prop_nodes_topo(batch,
-                                message_func=self.cell.message_func,
-                                reduce_func=self.cell.reduce_func,
-                                apply_node_func=self.cell.apply_node_func)
-            if self.top_down:
-                prop_nodes_topdown(batch,
-                                    message_func=self.cell_topdown.message_func,
-                                    reduce_func=self.cell_topdown.reduce_func,
-                                    apply_node_func=self.cell_topdown.apply_node_func)
-
-        if self.pool_mode=='root':
-            batch_pred=batch.ndata['h'][root_ids]
-        else:
-            batch_pred=self.pooling(batch,batch.ndata['h'])
-        nlbatch=nlbatch.transpose(0,1)
-        nl_encodings=self.nlencoder.encoding(nlbatch,src_key_padding_mask=nl_padding_mask)
-        scores=(nl_encodings[:,None,:]*batch_pred[None,:,:]).sum(-1)
-        loss_fct = nn.CrossEntropyLoss()
-        loss = loss_fct(scores, torch.arange(bs, device=scores.device))
-        return loss,batch_pred,nl_encodings
-
-class Transformer_codesearch(torch.nn.Module): #not used
-    def __init__(self, num_heads, dim_model, d_k, d_v, dim_hidden, vocab_size, dropout=0.2, num_layers=2):
-        super(Transformer_codesearch, self).__init__()
-        self.num_heads=num_heads
-        self.dim_model=dim_model
-        self.dropout = torch.nn.Dropout(dropout)
-        self.embeddings=nn.Embedding(vocab_size,dim_model)
-        self.num_layers=num_layers
-        self.pool_mode='attention'
-        print(self.pool_mode)
-        self.encoder=BasicTransformerEncoder(self.embeddings, dim_model, num_heads, dim_hidden, num_layers, dropout=dropout)
-        #self.nlencoder=BasicTransformerEncoder(self.embeddings, dim_model, num_heads, dim_hidden, num_layers, dropout=dropout)
-        self.init_weights()
-    
-    def init_weights(self):
-        initrange = 0.1
-        self.embeddings.weight.data.uniform_(-initrange, initrange)
-
-    def forward(self, codebatch,nlbatch,code_padding_mask,nl_padding_mask):
-        bs=nlbatch.size()[0]
-        codebatch=codebatch.transpose(0,1)
-        code_encodings=self.encoder.encoding(codebatch,src_key_padding_mask=code_padding_mask)
-        nlbatch=nlbatch.transpose(0,1)
-        nl_encodings=self.encoder.encoding(nlbatch,src_key_padding_mask=nl_padding_mask)
-        scores=(nl_encodings[:,None,:]*code_encodings[None,:,:]).sum(-1)
-        loss_fct = nn.CrossEntropyLoss()
-        loss = loss_fct(scores, torch.arange(bs, device=scores.device))
-        return loss,code_encodings,nl_encodings
