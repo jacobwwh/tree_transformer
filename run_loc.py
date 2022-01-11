c93363842f5b80afc766947cc29c6cbbdf5e5e3c
Wang Wenhan <wwhjacob@hotmail.com>
2022-01-11 16:01:08 +0800

    Update run_loc.py

--- run_loc.py
+++ run_loc.py
@@ -15,14 +15,25 @@ import torch
 import torch.nn as nn
 import dgl
 import torchtext
-from torchtext.data.utils import get_tokenizer
-from torch.utils.data import DataLoader
-from torch.optim.lr_scheduler import ReduceLROnPlateau
 from graph_utils import create_loc_dataset,create_loc_batch
-from decoder.enc_dec_utils import Tree2SeqDataset,collect_fn,label_smoothing_loss,calculate
 from utils import dataiterator
 from treetransformernew import TreeTransformer_localize
-from model.gnn_baseline import GNN_localize
+
+def boolean_string(s):
+    if s not in {'False', 'True'}:
+        raise ValueError('Not a valid boolean string')
+    return s == 'True'
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--dataset_name", type=str, default='java250, help="dataset name")
+parser.add_argument("--emsize", type=int, default=256, help="embedding dim")
+parser.add_argument("--num_heads", type=int, default=4, help="attention heads")
+parser.add_argument("--dropout", type=float, default=0.2)
+parser.add_argument("--lr", type=float, default=0.002, help="learning rate of adam")
+parser.add_argument("--batch_size", type=int, default=512, help="batch size")
+parser.add_argument("--maxepoch", type=int, default=30, help="max training epochs")
+parser.add_argument("--nobar", type=boolean_string, default=False, help="disable progress bar")
+args = parser.parse_args()
 
 def read_data_from_disk(path):
     files = os.listdir(path)
@@ -62,25 +73,19 @@ testset=read_data_from_disk('wrongoperator/test/')
 vocab=pickle.load(open('wrongoperator/vocab_min5.pkl','rb'))
 vocabsize=len(vocab)
 
-emsize = 256 # embedding dimension
-nhid = 256 # the dimension of the feedforward network model in nn.TransformerEncoder
-dk=32 #key dimension
-dv=32 #value dimension
-nlayers = 1 # the number of Transformer Encoder Layers
-nhead = 4 # the number of heads in the multiheadattention models
-dropout = 0.2 # the dropout value
+#emsize = 256 # embedding dimension
+nhid = args.emsize*4 # the dimension of feedforward layer
+#nhead = 4 # the number of heads in the multiheadattention models
+#dropout = 0.2 # the dropout value
 
-print('embedsize:',emsize,'hidden:',nhid,'key:',dk,'value:',dv,'layers:',nlayers,'heads:',nhead)
-
-model = TreeTransformer_localize(nhead,emsize,emsize,vocabsize).to(device)
+model = TreeTransformer_localize(args.num_heads,args.emsize,nhid,vocabsize).to(device)
 
 trainset=create_loc_dataset(trainset,vocab)
 devset=create_loc_dataset(devset,vocab)
 testset=create_loc_dataset(testset,vocab)
 
 criterion = nn.NLLLoss()
-lr=0.002
-optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 
 from torch.optim.lr_scheduler import _LRScheduler
 class NoamLR(_LRScheduler):
@@ -95,26 +100,21 @@ class NoamLR(_LRScheduler):
 
 warmup_steps=2000
 scheduler=NoamLR(optimizer,warmup_steps=warmup_steps)
-batch_size=64
-print(batch_size,lr,warmup_steps)
 
-
-nobar=False
 maxdevacc=0
 maxdevepoch=0
-for epoch in range(300):
+for epoch in range(args.maxepoch):
     print('epoch:',epoch+1)
     sys.stdout.flush()
     model.train()
     random.shuffle(devset)
-    trainbar=tqdm(dataiterator(devset,batch_size=batch_size),disable=nobar)
+    trainbar=tqdm(dataiterator(devset,batch_size=args.batch_size),disable=args.nobar)
     totalloss=0.0
     traincorrect=0
     for batch in trainbar:
         optimizer.zero_grad()
         inputbatch,targets,batch_maxlen,batch_numnodes=create_loc_batch(batch,device)
         output = model(inputbatch,batch_maxlen,batch_numnodes)
-        #print(output.size(),targets.size())
         loss = criterion(output, targets)
         trainbar.set_description("loss {}".format(loss.item()))
         totalloss+=loss.item()
@@ -128,7 +128,7 @@ for epoch in range(300):
     
     model.eval()
     with torch.no_grad():
-        devbar=tqdm(dataiterator(devset,batch_size=batch_size),disable=args.nobar)
+        devbar=tqdm(dataiterator(devset,batch_size=args.batch_size),disable=args.nobar)
         devtotal=len(devset)
         devcorrect=0
         for batch in devbar:
@@ -136,7 +136,7 @@ for epoch in range(300):
             output = model(inputbatch,batch_maxlen,batch_numnodes)
             devcorrect+=(output.argmax(1) == targets).sum().item()
         print('devacc:',devcorrect/devtotal)
-        testbar=tqdm(dataiterator(testset,batch_size=batch_size), disable=args.nobar)
+        testbar=tqdm(dataiterator(testset,batch_size=args.batch_size), disable=args.nobar)
         testtotal=len(testset)
         testcorrect=0
         for batch in testbar:
