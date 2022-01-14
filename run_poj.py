689e05bea85f1ae8ac597a51c5039678c8c72209
Wang Wenhan <wwhjacob@hotmail.com>
2022-01-14 13:42:14 +0800

    Update run_poj.py

--- run_poj.py
+++ run_poj.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 import torch
 import torch.nn as nn
 from graph_utils import create_graph_dataset, create_graph_batch
-from utils import dataiterator
+from utils import dataiterator,NoamLR
 from treetransformernew import TreeTransformerClassifier as newclassifier
 
 def boolean_string(s):
@@ -58,19 +58,6 @@ model = newclassifier(nhead,emsize,nhid,nclasses,vocabsize,dropout=dropout).to(d
 
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
-#scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
-
-from torch.optim.lr_scheduler import _LRScheduler
-class NoamLR(_LRScheduler):
-    def __init__(self, optimizer, warmup_steps):
-        self.warmup_steps = warmup_steps
-        super().__init__(optimizer)
-
-    def get_lr(self):
-        last_epoch = max(1, self.last_epoch)
-        scale = self.warmup_steps ** 0.5 * min(last_epoch ** (-0.5), last_epoch * self.warmup_steps ** (-1.5))
-        #scale=min(last_epoch/self.warmup_steps, 1)
-        return [base_lr * scale for base_lr in self.base_lrs]
 
 warmup_steps=2000
 scheduler=NoamLR(optimizer,warmup_steps=warmup_steps)
