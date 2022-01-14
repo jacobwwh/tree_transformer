0496f9bef6514e572c31f0789b5439b243b8bdbb
Wang Wenhan <wwhjacob@hotmail.com>
2022-01-14 13:41:28 +0800

    Update run_loc.py

--- run_loc.py
+++ run_loc.py
@@ -16,7 +16,7 @@ import torch.nn as nn
 import dgl
 import torchtext
 from graph_utils import create_loc_dataset,create_loc_batch
-from utils import dataiterator
+from utils import dataiterator,NoamLR
 from treetransformernew import TreeTransformer_localize
 
 def boolean_string(s):
@@ -85,17 +85,6 @@ testset=create_loc_dataset(testset,vocab)
 
 criterion = nn.NLLLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
-
-from torch.optim.lr_scheduler import _LRScheduler
-class NoamLR(_LRScheduler):
-    def __init__(self, optimizer, warmup_steps):
-        self.warmup_steps = warmup_steps
-        super().__init__(optimizer)
-
-    def get_lr(self):
-        last_epoch = max(1, self.last_epoch)
-        scale = self.warmup_steps ** 0.5 * min(last_epoch ** (-0.5), last_epoch * self.warmup_steps ** (-1.5))
-        return [base_lr * scale for base_lr in self.base_lrs]
 
 warmup_steps=2000
 scheduler=NoamLR(optimizer,warmup_steps=warmup_steps)
