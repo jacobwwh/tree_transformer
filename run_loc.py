67bc25b1c79c97d62a5ecd35ff46b1a90aac1a8a
Wang Wenhan <wwhjacob@hotmail.com>
2022-05-19 14:30:46 +0800

    Update run_loc.py

--- run_loc.py
+++ run_loc.py
@@ -15,7 +15,7 @@ import torch
 import torch.nn as nn
 import dgl
 import torchtext
-from graph_utils import create_loc_dataset,create_loc_batch
+from graph_utils import create_loc_dataset,create_loc_batch,create_loc_repair_batch
 from utils import dataiterator,NoamLR
 from treetransformernew import TreeTransformer_localize
 
@@ -34,6 +34,8 @@ parser.add_argument("--maxepoch", type=int, default=30, help="max training epoch
 parser.add_argument("--nobar", type=boolean_string, default=False, help="disable progress bar")
 args = parser.parse_args()
 
+ops_dict={'-':0,'+':1,'*':2,'%':3,'>':4,'==':5,'or':6,'<':7,'/':8,'and':9,'>=':10,'<=':11,'!=':12,'in':13,'is':14,'is not':15,'not in':16}
+
 def read_data_from_disk(path):
     files = os.listdir(path)
     importer = JsonImporter()
@@ -48,19 +50,8 @@ def read_data_from_disk(path):
                 for line in lines:
                     data=json.loads(line)
                     root = importer.import_(data['tree'])
-                    overlength=False
-                    cursor=root
-                    while cursor.children:
-                        cursor=cursor.children[-1]
-                        if cursor.index>2000:
-                            overlength=True
-                            overlencount+=1
-                            #print(cursor)
-                            break    
-                    #print(data['label'])
-                    opnodes=findall(root, lambda node: node.is_operator==True)
-                    if overlength==False and len(opnodes)>1:
-                        dataset.append({'tree':root})
+                    label=data['correct_op']
+                    dataset.append({'tree':root,'label':ops_dict[label]})
     print(len(dataset))
     return dataset
 
@@ -79,9 +70,9 @@ nhid = args.emsize*4 # the dimension of feedforward layer
 
 model = TreeTransformer_localize(args.num_heads,args.emsize,nhid,vocabsize).to(device)
 
-trainset=create_loc_dataset(trainset,vocab)
-devset=create_loc_dataset(devset,vocab)
-testset=create_loc_dataset(testset,vocab)
+trainset=create_loc_dataset(trainset,vocab,withlabel=True)
+devset=create_loc_dataset(devset,vocab,withlabel=True)
+testset=create_loc_dataset(testset,vocab,withlabel=True)
 
 criterion = nn.NLLLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
@@ -99,43 +90,57 @@ for epoch in range(args.maxepoch):
     trainbar=tqdm(dataiterator(devset,batch_size=args.batch_size),disable=args.nobar)
     totalloss=0.0
     traincorrect=0
+    traincorrect_locrepair=0
     for batch in trainbar:
         optimizer.zero_grad()
-        inputbatch,targets,batch_maxlen,batch_numnodes=create_loc_batch(batch,device)
-        output = model(inputbatch,batch_maxlen,batch_numnodes)
-        loss = criterion(output, targets)
-        trainbar.set_description("loss {}".format(loss.item()))
+        inputbatch,targets,batch_maxlen,batch_numnodes,repair_labels=create_loc_repair_batch(batch,device)
+        output,repair_output = model(inputbatch,batch_maxlen,batch_numnodes,repair=True)
+        loc_loss = criterion(output, targets)
+        repair_loss=repair_criterion(repair_output,repair_labels)
+        trainbar.set_description("loc loss {}, repair loss {}".format(loc_loss.item(),repair_loss.item()))
+        loss=loc_loss+repair_loss
         totalloss+=loss.item()
         loss.backward()
         torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
         optimizer.step()
         scheduler.step()
         traincorrect+=(output.argmax(1) == targets).sum().item()
-    print('avg loss:',totalloss/len(devset)*batch_size)
-    print('train acc:',traincorrect/len(devset))
+        traincorrect_locrepair+=((output.argmax(1) == targets)&(repair_output.argmax(1)==repair_labels)).sum().item()
+    print('avg loss:',totalloss/len(trainset)*batch_size)
+    print('train acc:',traincorrect/len(trainset))
+    print('train acc jointly loc&repair:',traincorrect_locrepair/len(trainset))
     
     model.eval()
     with torch.no_grad():
-        devbar=tqdm(dataiterator(devset,batch_size=args.batch_size),disable=args.nobar)
-        devtotal=len(devset)
+        devbar=tqdm(dataiterator(devset,batch_size=batch_size),disable=nobar)
         devcorrect=0
+        devcorrect_locrepair=0
         for batch in devbar:
-            inputbatch,targets,batch_maxlen,batch_numnodes=create_loc_batch(batch,device)
-            output = model(inputbatch,batch_maxlen,batch_numnodes)
+            inputbatch,targets,batch_maxlen,batch_numnodes,repair_labels=create_loc_repair_batch(batch,device)
+            output,repair_output = model(inputbatch,batch_maxlen,batch_numnodes,repair=True)
             devcorrect+=(output.argmax(1) == targets).sum().item()
-        print('devacc:',devcorrect/devtotal)
-        testbar=tqdm(dataiterator(testset,batch_size=args.batch_size), disable=args.nobar)
-        testtotal=len(testset)
-        testcorrect=0
-        for batch in testbar:
-            inputbatch,targets,batch_maxlen,batch_numnodes=create_loc_batch(batch,device)
-            output = model(inputbatch,batch_maxlen,batch_numnodes)
-            testcorrect+=(output.argmax(1) == targets).sum().item()
-        print('testacc:',testcorrect/testtotal)
-    if devcorrect/devtotal>=maxdevacc:
-        maxdevacc=devcorrect/devtotal
-        maxdevepoch=epoch
-        print('best epoch')
+            devcorrect_locrepair+=((output.argmax(1) == targets)&(repair_output.argmax(1)==repair_labels)).sum().item()
+        devacc=devcorrect/len(devset)
+        devacc_locrepair=devcorrect_locrepair/len(devset)
+        print('dev acc:',devacc)
+        print('dev acc jointly loc&repair:',devacc_locrepair)
+        if devacc>maxdevacc:
+            maxdevacc=devacc
+            maxdevepoch=epoch
+            print('best epoch')
+            print('start testing')
+            testbar=tqdm(dataiterator(testset,batch_size=batch_size),disable=nobar)
+            testcorrect=0
+            testcorrect_locrepair=0
+            for batch in testbar:
+                inputbatch,targets,batch_maxlen,batch_numnodes,repair_labels=create_loc_repair_batch(batch,device)
+                output,repair_output = model(inputbatch,batch_maxlen,batch_numnodes,repair=True)
+                testcorrect+=(output.argmax(1) == targets).sum().item()
+                testcorrect_locrepair+=((output.argmax(1) == targets)&(repair_output.argmax(1)==repair_labels)).sum().item()
+            testacc=testcorrect/len(testset)
+            testacc_locrepair=testcorrect_locrepair/len(testset)
+            print('test acc:',testacc)
+            print('test acc jointly loc&repair:',testacc_locrepair)
     if epoch-maxdevepoch>10:
         print('early stop')
         print('best epoch:',maxdevepoch)
